014b9acc582a0f70b5fa6c46105d08c5a28aad67
GARGI JAIN <jaingargigaps@gmail.com>
2023-04-21 23:16:42 +0530

    Gargi jain added homepage data

---
+++ homepage.php
@@ -0,0 +1,63 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="homepage.css">
+    </head>
+    <title>
+    </title>
+    <body>
+      <div class="app">
+        <div class="boxes">
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+        <div class="text">
+          <h1>
+            <span>Let's</span>
+            <span>talk</span>
+            <span>about</span>
+            <span>mental</span>
+            <span>health</span>
+            <span>of</span>
+            <span>students</span>
+        </h1>
+        </div>
+
+      </div>
+      <div class="banner">
+          
+      </div>
+      <div class="image">
+          <div class="chakra">
+            <img src="chakra.png" height="773px">
+          </div>
+          <div class="crown">
+            <img src="crown.png" height="80px">
+          </div>
+          <div class="third_eye">
+            <img src="third_eye.png" height="40px">
+          </div>
+          <div class="throat">
+            <img src="throat.png" height="80px">
+          </div>
+          <div class="heart">
+            <img src="heart.png" height="90px">
+          </div>
+          <div class="solar_plexus">
+            <img src="solar_plexus.png" height="80px">
+          </div>
+          <div class="sacral">
+            <img src="sacral.png" height="80px">
+          </div>
+          <div class="root">
+            <img src="root.png" height="80px">
+          </div>
+        </div>
+    </body>
+</html>

862130d8e53e07d34997b33e7ba50f35d1ee12da
GARGI JAIN <jaingargigaps@gmail.com>
2023-04-22 23:31:49 +0530

    changed statistics

--- statistics.js
+++ statistics.js
@@ -1,80 +1,311 @@
-window.onload = function () {
-
-    var chart = new CanvasJS.Chart("chartContainer", {
-        theme: "dark2",
-        exportEnabled: true,
-        animationEnabled: true,
-        title: {
-            text: "Suicides commited all over India"
-        },
-        subtitles: [{
-            text: "Click Legend to Hide or Unhide Data Series"
-        }],
-        axisX: {
-            title: "Years"
-        },
-        axisY: {
-            title: "Total Suicides",
-            titleFontColor: "#4F81BC",
-            lineColor: "#4F81BC",
-            labelFontColor: "#4F81BC",
-            tickColor: "#4F81BC",
-            includeZero: true
-        },
-        axisY2: {
-            title: "Suicides by Students",
-            titleFontColor: "#C0504E",
-            lineColor: "#C0504E",
-            labelFontColor: "#C0504E",
-            tickColor: "#C0504E",
-            includeZero: true
-        },
-        toolTip: {
-            shared: true
-        },
-        legend: {
-            cursor: "pointer",
-            itemclick: toggleDataSeries
-        },
-        data: [{
-            type: "column",
-            name: "Total Suicides",
-            showInLegend: true,
-            yValueFormatString: "#,##0.#",
-            dataPoints: [
-                { label: "2001", y: 19034.5 },
-                { label: "2002", y: 20015 },
-                { label: "2003", y: 25342 },
-                { label: "2004", y: 20088 },
-                { label: "2005", y: 28234 },
-                { label: "2006", y: 29234 }
-            ]
-        },
-        {
-            type: "column",
-            name: "Suicides by Students",
-            axisYType: "secondary",
-            showInLegend: true,
-            yValueFormatString: "#,##0.#",
-            dataPoints: [
-                { label: "2001", y: 210.5 },
-                { label: "2002", y: 135 },
-                { label: "2003", y: 425 },
-                { label: "2004", y: 130 },
-                { label: "2005", y: 528 },
-                { label: "2006", y: 428 }
-            ]
-        }]
-    });
-    chart.render();
-
-    function toggleDataSeries(e) {
-        if (typeof (e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
-            e.dataSeries.visible = false;
-        } else {
-            e.dataSeries.visible = true;
-        }
-        e.chart.render();
+window.addEventListener('load',function(){
+    var label = document.querySelector(".label");
+    var c = document.getElementById("c");
+    var ctx = c.getContext("2d");
+    
+    var cw = (c.width = 700);
+    var ch = (c.height = 350);
+    var cx = cw / 2,
+      cy = ch / 2;
+    var rad = Math.PI / 180;
+    var frames = 0;
+    
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = "#999";
+    ctx.fillStyle = "#ccc";
+    ctx.font = "14px monospace";
+    
+    var grd = ctx.createLinearGradient(0, 0, 0, cy);
+    grd.addColorStop(0, "hsla(167,72%,60%,1)");
+    grd.addColorStop(1, "hsla(167,72%,60%,0)");
+    
+    
+    var oData = {
+      2016: 9.5,
+      2017: 9.9,
+      2018: 10.1,
+      2019: 10.3,
+      2020: 12.5,
+      2021: 13.1,
+      2022: 13.7,
+      2023: 14.3,
+      2024: 15.0,
+      2025: 15.8,
+      2026: 16.5,
+      2027: 17.2,
+      2028: 18,
+      2029:18.7,
+      2030:19.5,
+    };
+    
+    var valuesRy = [];
+    var propsRy = [];
+    for (var prop in oData) {
+      valuesRy.push(oData[prop]);
+      propsRy.push(prop);
     }
-
-}
+    
+    var vData = 4;
+    var hData = valuesRy.length;
+    var offset =40; //offset chart axis
+    var chartHeight = ch - 2 * offset;
+    var chartWidth = cw - 2 * offset;
+    var t = 1 / 7; // curvature : 0 = no curvature
+    var speed = 2; // for the animation
+    
+    var A = {
+      x: offset,
+      y: offset
+    };
+    var B = {
+      x: offset,
+      y: offset + chartHeight
+    };
+    var C = {
+      x: offset + chartWidth,
+      y: offset + chartHeight
+    };
+    
+    /*
+          A  ^
+            |  |  
+            + 25
+            |
+            |
+            |
+            + 25  
+          |__|_________________________________  C
+          B
+    */
+    
+    // CHART AXIS -------------------------
+    ctx.beginPath();
+    ctx.moveTo(A.x, A.y);
+    ctx.lineTo(B.x, B.y);
+    ctx.lineTo(C.x, C.y);
+    ctx.stroke();
+    
+    // vertical ( A - B )
+    var aStep = (chartHeight - 50) / vData;
+    
+    var Max = Math.ceil(arrayMax(valuesRy) / 10) * 10;
+    var Min = Math.floor(arrayMin(valuesRy) / 10) * 10;
+    var aStepValue = (Max - Min) / vData;
+    console.log("aStepValue: " + aStepValue); //8 units
+    var verticalUnit = aStep / aStepValue;
+    
+    var a = [];
+    ctx.textAlign = "right";
+    ctx.textBaseline = "middle";
+    for (var i = 0; i <= vData; i++) {
+      if (i == 0) {
+        a[i] = {
+          x: A.x,
+          y: A.y + 25,
+          val: Max
+        };
+      } else {
+        a[i] = {};
+        a[i].x = a[i - 1].x;
+        a[i].y = a[i - 1].y + aStep;
+        a[i].val = a[i - 1].val - aStepValue;
+      }
+      drawCoords(a[i], 3, 0);
+    }
+    
+    //horizontal ( B - C )
+    var b = [];
+    ctx.textAlign = "center";
+    ctx.textBaseline = "hanging";
+    var bStep = chartWidth / (hData + 1);
+    
+    for (var i = 0; i < hData; i++) {
+      if (i == 0) {
+        b[i] = {
+          x: B.x + bStep,
+          y: B.y,
+          val: propsRy[0]
+        };
+      } else {
+        b[i] = {};
+        b[i].x = b[i - 1].x + bStep;
+        b[i].y = b[i - 1].y;
+        b[i].val = propsRy[i];
+      }
+      drawCoords(b[i], 0, 3);
+    }
+    
+    function drawCoords(o, offX, offY) {
+      ctx.beginPath();
+      ctx.moveTo(o.x - offX, o.y - offY);
+      ctx.lineTo(o.x + offX, o.y + offY);
+      ctx.stroke();
+    
+      ctx.fillText(o.val, o.x - 2 * offX, o.y + 2 * offY);
+    }
+    //----------------------------------------------------------
+    
+    // DATA
+    var oDots = [];
+    var oFlat = [];
+    var i = 0;
+    
+    for (var prop in oData) {
+      oDots[i] = {};
+      oFlat[i] = {};
+    
+      oDots[i].x = b[i].x;
+      oFlat[i].x = b[i].x;
+    
+      oDots[i].y = b[i].y - oData[prop] * verticalUnit - 25;
+      oFlat[i].y = b[i].y - 25;
+    
+      oDots[i].val = oData[b[i].val];
+    
+      i++;
+    }
+    
+    function animateChart() {
+      requestId = window.requestAnimationFrame(animateChart);
+      frames += speed; //console.log(frames)
+      ctx.clearRect(60, 0, cw, ch - 60);
+    
+      for (var i = 0; i < oFlat.length; i++) {
+        if (oFlat[i].y > oDots[i].y) {
+          oFlat[i].y -= speed;
+        }
+      }
+      drawCurve(oFlat);
+      for (var i = 0; i < oFlat.length; i++) {
+        ctx.fillText(oDots[i].val, oFlat[i].x, oFlat[i].y - 25);
+        ctx.beginPath();
+        ctx.arc(oFlat[i].x, oFlat[i].y, 3, 0, 2 * Math.PI);
+        ctx.fill();
+      }
+    
+      if (frames >= Max * verticalUnit) {
+        window.cancelAnimationFrame(requestId);
+      }
+    }
+    requestId = window.requestAnimationFrame(animateChart);
+    
+    c.addEventListener(
+      "mousemove",
+      function (e) {
+        label.innerHTML = "";
+        label.style.display = "none";
+        this.style.cursor = "default";
+    
+        var m = oMousePos(this, e);
+        for (var i = 0; i < oDots.length; i++) {
+          output(m, i);
+        }
+      },
+      false
+    );
+    
+    function output(m, i) {
+      ctx.beginPath();
+      ctx.arc(oDots[i].x, oDots[i].y, 20, 0, 2 * Math.PI);
+      if (ctx.isPointInPath(m.x, m.y)) {
+        //console.log(i);
+        label.style.display = "block";
+        label.style.top = m.y + 10 + "px";
+        label.style.left = m.x + 10 + "px";
+        label.innerHTML =
+          "<strong>" + propsRy[i] + "</strong>: " + valuesRy[i] + "%";
+        c.style.cursor = "pointer";
+      }
+    }
+    
+    function controlPoints(p) {
+      // given the points array p calculate the control points
+      var pc = [];
+      for (var i = 1; i < p.length - 1; i++) {
+        var dx = p[i - 1].x - p[i + 1].x; // difference x
+        var dy = p[i - 1].y - p[i + 1].y; // difference y
+        // the first control point
+        var x1 = p[i].x - dx * t;
+        var y1 = p[i].y - dy * t;
+        var o1 = {
+          x: x1,
+          y: y1
+        };
+    
+        // the second control point
+        var x2 = p[i].x + dx * t;
+        var y2 = p[i].y + dy * t;
+        var o2 = {
+          x: x2,
+          y: y2
+        };
+    
+        // building the control points array
+        pc[i] = [];
+        pc[i].push(o1);
+        pc[i].push(o2);
+      }
+      return pc;
+    }
+    
+    function drawCurve(p) {
+      var pc = controlPoints(p); // the control points array
+    
+      ctx.beginPath();
+      //ctx.moveTo(p[0].x, B.y- 25);
+      ctx.lineTo(p[0].x, p[0].y);
+      // the first & the last curve are quadratic Bezier
+      // because I'm using push(), pc[i][1] comes before pc[i][0]
+      ctx.quadraticCurveTo(pc[1][1].x, pc[1][1].y, p[1].x, p[1].y);
+    
+      if (p.length > 2) {
+        // central curves are cubic Bezier
+        for (var i = 1; i < p.length - 2; i++) {
+          ctx.bezierCurveTo(
+            pc[i][0].x,
+            pc[i][0].y,
+            pc[i + 1][1].x,
+            pc[i + 1][1].y,
+            p[i + 1].x,
+            p[i + 1].y
+          );
+        }
+        // the first & the last curve are quadratic Bezier
+        var n = p.length - 1;
+        ctx.quadraticCurveTo(pc[n - 1][0].x, pc[n - 1][0].y, p[n].x, p[n].y);
+      }
+    
+      //ctx.lineTo(p[p.length-1].x, B.y- 25);
+      ctx.stroke();
+      ctx.save();
+      ctx.fillStyle = grd;
+      ctx.fill();
+      ctx.restore();
+    }
+    
+    function arrayMax(array) {
+      return Math.max.apply(Math, array);
+    }
+    
+    function arrayMin(array) {
+      return Math.min.apply(Math, array);
+    }
+    
+    function oMousePos(canvas, evt) {
+      var ClientRect = canvas.getBoundingClientRect();
+      return {
+        //objeto
+        x: Math.round(evt.clientX - ClientRect.left),
+        y: Math.round(evt.clientY - ClientRect.top)
+      };
+    }
+    ctx.fillText("Years", cw / 2, ch - 10);
+    ctx.save();
+    ctx.translate(10, ch / 2);
+    ctx.rotate(-Math.PI / 2);
+    ctx.font = "11px";
+    ctx.textAlign = "center";
+    ctx.fillText("Student suicide cases per year (in thousands)", 0, -10);
+    ctx.restore();
+    });
+    

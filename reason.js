2dc63c23710cd1494cd8b8ebe9f4ba534cb4478c
ARANYA MAJI <aranyamaji28@gmail.com>
2023-04-22 15:27:38 +0530

    reasons made

---
+++ reason.js
@@ -0,0 +1,13 @@
+function clclr(val)
+{
+    document.getElementById(val).style.color = "green";
+    document.getElementById(val).style.borderColor = "green";
+    document.getElementById(val).style.transitionDuration = "0.5s";
+}
+
+function clrbk(val)
+{
+    document.getElementById(val).style.color = "white";
+    document.getElementById(val).style.borderColor = "white";
+    document.getElementById(val).style.transitionDuration = "0.5s";
+}

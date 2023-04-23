ce2d69c7e50b0e7d8280c5678c101678f376b0de
ARANYA MAJI <aranyamaji28@gmail.com>
2023-04-23 07:49:03 +0530

    reason updated

--- reason.js
+++ reason.js
@@ -20,4 +20,20 @@ function op(val)
     document.getElementById("box2").style.opacity = "100%";
     document.getElementById("box1").style.transitionDuration = "1s";
     document.getElementById("box2").style.transitionDuration = "1s";
+    const dialogues = ["ap","si","fs","rp","t","sa","g","b"];
+    document.getElementById("para").innerHTML = dialogues[val-1];
+}
+
+function clk()
+{
+    document.getElementById("box1").style.transitionDuration = "1s";
+    document.getElementById("box2").style.transitionDuration = "1s";
+    document.getElementById("box1").style.opacity = "0%";
+    document.getElementById("box2").style.opacity = "0%";
+}
+
+function rem()
+{
+    document.getElementById("details").style.transitionDuration = "0.5s";
+    document.getElementById("details").style.opacity = "0%";
 }

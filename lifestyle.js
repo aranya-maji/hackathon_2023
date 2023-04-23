fb65698f49e8bf0c3fec4494bf3b95a8a482831e
GARGI JAIN <jaingargigaps@gmail.com>
2023-04-23 18:48:51 +0530

    lifestyle

---
+++ lifestyle.js
@@ -0,0 +1,84 @@
+function abl()
+{
+    document.getElementById("lovers").style.borderColor="rgba(255,255,255,0.2)";
+    document.getElementById("textL").style.opacity="100%";
+    document.getElementById("lovers").style.transitionDuration="0.5s";
+    document.getElementById("textL").style.transitionDuration="0.5s";
+}
+ function bkl()
+{
+    document.getElementById("lovers").style.borderColor="white";
+    document.getElementById("textL").style.opacity="0%";
+    document.getElementById("lovers").style.transitionDuration="0.5s";
+    document.getElementById("textL").style.transitionDuration="0.5s";
+}
+function abo()
+{
+    document.getElementById("lovers").style.borderColor="rgba(255,255,255,0.2)";
+    document.getElementById("textO").style.opacity="100%";
+    document.getElementById("lovers").style.transitionDuration="0.5s";
+    document.getElementById("textO").style.transitionDuration="0.5s";
+}
+ function bko()
+{
+    document.getElementById("lovers").style.borderColor="white";
+    document.getElementById("textO").style.opacity="0%";
+    document.getElementById("lovers").style.transitionDuration="0.5s";
+    document.getElementById("textO").style.transitionDuration="0.5s";
+}
+function abv()
+{
+    document.getElementById("lovers").style.borderColor="rgba(255,255,255,0.2)";
+    document.getElementById("textV").style.opacity="100%";
+    document.getElementById("lovers").style.transitionDuration="0.5s";
+    document.getElementById("textV").style.transitionDuration="0.5s";
+}
+ function bkv()
+{
+    document.getElementById("lovers").style.borderColor="white";
+    document.getElementById("textV").style.opacity="0%";
+    document.getElementById("lovers").style.transitionDuration="0.5s";
+    document.getElementById("textV").style.transitionDuration="0.5s";
+}
+function abe()
+{
+    document.getElementById("lovers").style.borderColor="rgba(255,255,255,0.2)";
+    document.getElementById("textE").style.opacity="100%";
+    document.getElementById("lovers").style.transitionDuration="0.5s";
+    document.getElementById("textE").style.transitionDuration="0.5s";
+}
+ function bke()
+{
+    document.getElementById("lovers").style.borderColor="white";
+    document.getElementById("textE").style.opacity="0%";
+    document.getElementById("lovers").style.transitionDuration="0.5s";
+    document.getElementById("textE").style.transitionDuration="0.5s";
+}
+function abr()
+{
+    document.getElementById("lovers").style.borderColor="rgba(255,255,255,0.2)";
+    document.getElementById("textR").style.opacity="100%";
+    document.getElementById("lovers").style.transitionDuration="0.5s";
+    document.getElementById("textR").style.transitionDuration="0.5s";
+}
+ function bkr()
+{
+    document.getElementById("lovers").style.borderColor="white";
+    document.getElementById("textR").style.opacity="0%";
+    document.getElementById("lovers").style.transitionDuration="0.5s";
+    document.getElementById("textR").style.transitionDuration="0.5s";
+}
+function abs()
+{
+    document.getElementById("lovers").style.borderColor="rgba(255,255,255,0.2)";
+    document.getElementById("textS").style.opacity="100%";
+    document.getElementById("lovers").style.transitionDuration="0.5s";
+    document.getElementById("textS").style.transitionDuration="0.5s";
+}
+ function bks()
+{
+    document.getElementById("lovers").style.borderColor="white";
+    document.getElementById("textS").style.opacity="0%";
+    document.getElementById("lovers").style.transitionDuration="0.5s";
+    document.getElementById("textS").style.transitionDuration="0.5s";
+}
